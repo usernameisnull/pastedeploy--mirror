fbcb467caa37f7465723c56fa39c5f67fdfadc11
mabing <cumt_ttr@163.com>
2017-04-05 21:17:36 +0800

    解决部分中文乱码, 添加一些注释, 和一个问题在MABING.txt里

--- tests/mabing.py
+++ tests/mabing.py
@@ -4,4 +4,4 @@ here = "."
 result = loadapp('config:sample_configs/basic_app.ini', relative_to=here)
 print result, result.__class__
 import inspect
-print inspect.getsource(result)
+print inspect.getsource(result)  # 这里直接返回basic_app这个函数的源码
